e275e5e2b913efcffc5f4d16cea39d23b72f5e10
Nicolas Morales <nicolasmoralesj03@gmail.com>
2025-08-07 18:43:56 -0300

    primera pagina

---
+++ app/tableros/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const metadata = {
+ title: 'SEO Title',
+ description: 'SEO Title',
+};
+
+const tableros = () => {
+  return (
+    <div>tablero</div>
+  )
+}
+
+export default tableros
